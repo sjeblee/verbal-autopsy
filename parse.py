28beaf13ed98a8481dfda6a4800c6dd3fc6fb369
Serena Jeblee <sjeblee@cs.toronto.edu>
2016-11-11 17:25:51 -0500

    Added parse.py to run the Stanford parser on the narratives
    * This doesn't work yet because of some dependency issues with other Stanford components

---
+++ parse.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+# Add parses to the narrative
+
+from lxml import etree
+import argparse
+import numpy
+import string
+import subprocess
+
+def main():
+    argparser = argparse.ArgumentParser()
+    argparser.add_argument('--in', action="store", dest="infile")
+    argparser.add_argument('--out', action="store", dest="outfile")
+    args = argparser.parse_args()
+
+    if not (args.infile and args.outfile):
+        print "usage: ./parse.py --in [file.xml] --out [outfile.xml]"
+        exit()
+
+    # Get the xml from file
+    tree = etree.parse(args.infile)
+    root = tree.getroot()
+    
+    for child in root:
+        node = child.find("narrative")
+        narr = ""
+        if node != None:
+            narr = node.text.encode('utf-8')
+        if len(narr) > 0:
+            tempname = "/u/sjeblee/research/va/data/temp.txt"
+            temp = open(tempname, "w")
+            temp.write(narr)
+            temp.close()
+            # Run Stanford parser on narr
+            process = subprocess.Popen(["java", "-cp", "/p/spoclab/tools/Stanford/stanford-corenlp-full-2015-04-20/stanford-corenlp-3.5.2.jar", "edu.stanford.nlp.parser.nndep.DependencyParser", "-model", "/u/sjeblee/research/va/res/stanford/english_SD.gz", "-textFile", tempname, "-outFile", "-"], stdout=subprocess.PIPE)
+            output, err = process.communicate()
+            newnode = etree.Element("narr_parsed")
+            newnode.text = output.decode('utf-8')
+            child.append(newnode)
+        
+    # write the stats to file
+    tree.write(args.outfile)
+    
+
+if __name__ == "__main__":main()
