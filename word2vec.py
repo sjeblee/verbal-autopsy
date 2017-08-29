960f8c1208b58bdbfb359c4992de4900b6eead14
Serena Jeblee <sjeblee@cs.toronto.edu>
2017-08-29 14:12:38 -0400

    Update word2vec.py to train on medhelp, ICE, and training data

--- word2vec.py
+++ word2vec.py
@@ -2,49 +2,74 @@
 # -*- coding: utf-8 -*-
 # Train word2vec model
 
+import argparse
+import os
+import subprocess
+import extract_features
+import preprocessing
+
 def main():
     argparser = argparse.ArgumentParser()
     argparser.add_argument('--in', action="store", dest="infile")
     argparser.add_argument('--size', action="store", dest="vec_size")
     args = argparser.parse_args()
 
-    if not (args.infile and args.outfile):
-        print "usage: ./word2vec.py --size [vector_size] --in [train_narrs.txt]"
+    if not (args.infile and args.vec_size):
+        print "usage: ./word2vec.py --size [vector_size] --in [train_xxx.xml]"
         exit()
 
     run(args.infile, args.vec_size)
 
 def run(infile, vec_size):
 
-    bin_dir = "~/tools/word2vec/word2vec/bin"
+    bin_dir = "/u/sjeblee/tools/word2vec/word2vec/bin"
 
     # Input data
-    data_dir = "~/research/va/data/datasets/mds+rct"
-    ice_data = "~/research/va/res/ICE-India/Corpus/all-final.txt"
-    medhelp_data = "~/research/va/tools/medhelp-crawler/data/posts.txt"
+    data_dir = "/u/sjeblee/research/va/data/datasets/mds+rct"
+    ice_data = "/u/sjeblee/research/va/res/ICE-India/Corpus/all-lower-stem.txt"
+    medhelp_data = "/u/sjeblee/research/data/medhelp/all_medhelp_clean_stem.txt"
 
-    suffix = "cat_spell.narrsent"
+    suffix = ".narrsent"
 
     # Output data
-    text_data = data_dir + "/all_narr.txt"
-    vec_data = data_dir + "/narr+ice.vectors"
+    text_data = data_dir + "/narr+ice+medhelp_stem.txt"
+    vec_data = data_dir + "/narr+ice+medhelp_stem.vectors." + str(vec_size) 
 
-    #cat $DATA_DIR/train_all_$SUFFIX $ICE_DATA $MEDHELP_DATA > $TEXT_DATA
+    # Extract narrative text from input file
+    narrs = extract_features.get_narrs(infile)
+    train_data = infile + suffix
+    outfile = open(train_data, "w")
+    for narr in narrs:
+        # Stemming
+        narr = preprocessing.stem(narr)
+        outfile.write(narr + "\n")
+    outfile.close()
+
+    # Combine all the text
+    filenames = [ice_data, medhelp_data, train_data]
+    outfile = open(text_data, "w")
+    for fname in filenames:
+        with open(fname) as inf:
+            for line in inf:
+                outfile.write(line)
+    outfile.close()
 
     #rm -f $VECTOR_DATA
-    window_size = 5
-    num_threads = 12
+    window_size = "5"
+    num_threads = "12"
 
     if not os.path.exists(vec_data):
         print "--------------------------------------------------------------------"
         print "-- Training vectors..."
-        process = subprocess.Popen(["time", bin_dir + "/word2vec", "-train", text_data, "-output", vec_data, "-cbow", 0, "-size", vec_size, "-window", window_size, "-negative", "0", "-hs", "1", "-min-count", "1", "-sample", "1e-3", "-threads", num_threads, "-binary", "0"], stdout=subprocess.PIPE)
+        process = subprocess.Popen(["time", bin_dir + "/word2vec", "-train", text_data, "-output", vec_data, "-cbow", "0", "-size", str(vec_size), "-window", window_size, "-negative", "0", "-hs", "1", "-min-count", "1", "-sample", "1e-3", "-threads", num_threads, "-binary", "0"], stdout=subprocess.PIPE)
         output, err = process.communicate()
+        print output
         #time $BIN_DIR/word2vec -train $TEXT_DATA -output $VECTOR_DATA -cbow 0 -size $DIM -window 5 -negative 0 -hs 1 -min-count 1 -sample 1e-3 -threads 12 -binary 0
 
         print "-- Training binary vectors..."
-        process = subprocess.Popen(["time", bin_dir + "/word2vec", "-train", text_data, "-output", vec_data, "-cbow", 0, "-size", vec_size, "-window", window_size, "-negative", "0", "-hs", "1", "-min-count", "1", "-sample", "1e-3", "-threads", num_threads, "-binary", "1"], stdout=subprocess.PIPE)
+        process = subprocess.Popen(["time", bin_dir + "/word2vec", "-train", text_data, "-output", vec_data + ".bin", "-cbow", "0", "-size", str(vec_size), "-window", window_size, "-negative", "0", "-hs", "1", "-min-count", "1", "-sample", "1e-3", "-threads", num_threads, "-binary", "1"], stdout=subprocess.PIPE)
         output, err = process.communicate()
+        print output
         #time $BIN_DIR/word2vec -train $TEXT_DATA -output $VECTOR_DATA.bin -cbow 0 -size $DIM -window 5 -negative 0 -hs 1 -min-count 1 -sample 1e-3 -threads 12 -binary 1
   
 
