3073585ad43b312161642f53bb110ce2aa7dd695
Serena Jeblee <sjeblee@cs.toronto.edu>
2017-10-16 11:46:04 -0400

    Add cleanfile.sh, a script to do various cleaning on xml files

---
+++ cleanfile.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+file=$1
+outfile=$2
+
+# remove blank lines | remove extra spaces| remove leading and trailing spaces  | fix utf-8 chars
+sed '/^\s*$/d' $file | sed -e 's/  */ /g' | sed -e 's/^ //g' | sed -e 's/ $//g' | sed -e 's/&amp;/and/g' | sed -e 's/&#13;/ /g' | sed -e "s/&#8217;/\'/g" | sed -e 's/&#8221;/"/g' | sed -e 's/&#8220;/"/g' > $outfile
