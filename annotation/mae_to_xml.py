fbe081ec4927cb06c15d6dc579eef8291856b519
Serena Jeblee <sjeblee@cs.toronto.edu>
2017-11-26 16:34:29 -0500

    Update annotation conversion scripts
    *mae_to_xml should work for both MDS and i2b2

--- annotation/mae_to_xml.py
+++ annotation/mae_to_xml.py
@@ -4,7 +4,32 @@
 
 from lxml import etree
 import argparse
+import os
 import string
+import subprocess
+
+class Tag:
+    def __init__(self, name, tid, start, end, text, attributes=""):
+        self.name = name
+        self.tid = tid
+        if start == '':
+            self.start = None
+        else:
+            self.start = int(start)
+        if end == '':
+            self.end = None
+        else:
+            self.end = int(end)
+        self.text = text
+        self.attributes = attributes
+
+    def __str__(self):
+        string = ""
+        if self.name == "TLINK":
+            string = '<' + self.name + ' id="' + self.tid + '" ' + self.attributes + '/>'
+        else:
+            string = '<' + self.name + ' id="' + self.tid + '" ' + self.attributes + '> ' + self.text + ' </' + self.name + '>'
+        return string
 
 def main():
     argparser = argparse.ArgumentParser()
@@ -13,120 +38,206 @@ def main():
     argparser.add_argument('--dir', action="store", dest="ann_dir")
     args = argparser.parse_args()
 
-    if not (args.infile and args.ann_dir):
-        print "usage: ./xml_to_mae_in.py --dir [path/to/dir] --in [file.xml] --out [file.xml]"
+    if not (args.outfile and args.ann_dir):
+        print "usage: ./mae_to_mae_in.py --dir [path/to/dir] --in [file.xml] --out [file.xml]"
         exit()
 
-    run(args.infile, args.outfile, args.ann_dir)
+    arg_infile = ""
+    if args.infile:
+        arg_infile = args.infile
+
+    run(arg_infile, args.outfile, args.ann_dir)
 
 def run(arg_infile, arg_outfile, arg_dir):
 
-    # TODO: get id from filename, store in dictionary
     ann_narrs = {} # map from record id to annotated narrative
-    # for file in arg_dir:
-    # TODO: save file contents in ann_narrs[id] = text
+    for filename in os.listdir(arg_dir):
+        temp = filename.split(".")[0]
+        #print "temp: " + temp
+        if '_' in temp:
+            parts = temp.split("_")
+            #print "parts: " + str(parts)
+            record_name = parts[0]
+        else:
+            record_name = temp
+        print "record_id: " + record_name
+        with open(arg_dir + '/' + filename, 'r') as f:
+            text = f.read()
+            xml_text = convert_spans_to_xml(text)
+            ann_narrs[record_name] = xml_text
 
-    # TODO: for each record in infile, find the annotated output, put in it in the narr_timeml_simple node
+    # Add the annotated narrative to the xml file
+    tree = None
+    if arg_infile == "":
+        root = etree.Element("Record")
+        tree = etree.ElementTree(root)
+        for an in ann_narrs:
+            child = etree.Element("Adult_Anonymous")
+            rec_id = an
+            narr = ann_narrs[an]
+            id_node = etree.Element("MG_ID")
+            id_node.text = rec_id
+            child.append(id_node)
+            narr_node = etree.Element("narr_timeml_simple")
+            narr_node.text = narr.decode('utf-8')
+            child.append(narr_node)
+            root.append(child)
+    else:
+        tree = etree.parse(arg_infile)
+        root = tree.getroot()
+        for child in root:
+            id_node = child.find("MG_ID")
+            rec_id = id_node.text
+            narr_node = etree.Element("narr_timeml_simple")
+            narr = ann_narrs[rec_id]
+            narr_node.text = narr.decode('utf-8')
+            child.append(narr_node)
 
-    # Get the xml from file
-    tree = etree.parse(args.infile)
-    root = tree.getroot()
-    for child in root:
-        id_node = child.find("MG_ID")
-        rec_id = id_node.text
-        narr_text_node = child.find("narrative")
-        narr_text = narr_text_node.text
-        narr_node = child.find("narr_timeml_simple")
-        narr = etree.tostring(narr_node)
-        # Extract text from narrative
-        narr = narr[20:(len(narr)-21)]
-        print "narr: " + narr
-        x = 0
-        index = 0
-        narr_tags = ""
-        while x < len(narr):
-            
-            char = narr[x]
-            if char == '<':
-                startspan = index
-                x = x+1
-                if len(narr) > x+5:
-                    tag = narr[x:x+6]
-                    print "found tag: " + tag
-                    idnum = ""
-                    # Fix longer tags
-                    if tag == "TIMEX3" or tag == "SIGNAL":
-                        x = x+1
-                    tag = tag.strip()
-                    # Skip space and id tag name
-                    x = x+11
-                    # Get id
-                    end = narr.index('"', x)
-                    idnum = narr[x:end]
-                    x = end
-                    # Create tag string
-                    restoftag = ""
-                    char = narr[x]
-                    while char != '>' and x < len(narr):
-                        x = x+1
-                        char = narr[x]
-                        if char != ">":
-                            restoftag = restoftag + char
-                    if tag != "TLINK":
-                        # Calcuate span
-                        text = ""
-                        # Skip initial space
-                        x = x+1
-                        if narr_text[index] == " ":
-                            index = index+1
-                        print "x: " + str(x) + " i: " + str(index)
-                        print "narr[x]: '" + str(narr[x]) + "' narr[i]: '" + str(narr_text[index]) + "'"
-                        while char != '<' and x < len(narr):
-                            x = x+1
-                            index = index+1
-                            char = narr[x]
-                            text = text + char
-                        endspan = index-2
-                        index = index-2 # remove trailing space
-                        text = text[:-2]
-                        print "text from x: '" + text + "'"
-                        text = narr_text[startspan:endspan].strip()
-                        print "text from i: '" + text + "'"
-                        print "x: " + str(x) + " i: " + str(index)
-                        #narr_text = narr_text + text + " "
-                        narr_tags = narr_tags + '<' + tag + ' id="' + idnum + '" spans="' + str(startspan) + '~' + str(endspan) + '" text="' + text + '" ' + restoftag + "/>\n"
-                        # Skip over end tag
-                        while char != '>' and x < len(narr):
-                            x = x+1
-                            char = narr[x]
-                    else: # TLINK TODO: create TLINKs from event attributes
-                        from_start = restoftag.index('eventID') +9
-                        from_end = restoftag.index('"', from_start)
-                        from_id = restoftag[from_start:from_end]
-                        to_start = restoftag.index('relatedToTime') +15
-                        to_end = restoftag.index('"', to_start)
-                        to_id = restoftag[to_start:to_end]
-                        print "TLINK from: " + from_id + " to: " + to_id
-                        narr_tags = narr_tags + '<' + tag + ' id="' + idnum + '" from="' + from_id + '" to="' + to_id + '" ' + restoftag + ">\n"
-                    x = x+1 #Skip the space after the tag
-                    #char = narr[x]
-                    #if (char == " "):
-                    #    x = x+1
-                    #    char = narr[x]
-                    #    print "x: " + str(x) + " char: " + char
+    tree.write(arg_outfile)
+    subprocess.call(["sed", "-i", "-e", 's/&lt;/ </g', arg_outfile])
+    subprocess.call(["sed", "-i", "-e", 's/&gt;/> /g', arg_outfile])
+    subprocess.call(["sed", "-i", "-e", 's/  / /g', arg_outfile])
+
+def convert_spans_to_xml(text):
+    cdata_start = "<TEXT><![CDATA["
+    cdata_end = "]]></TEXT>"
+    tag_start = "<TAGS>"
+    tag_end = "</TAGS>"
+    lines = text.splitlines()
+    lines = lines[2:] # ignore the xml header
+    narr_text = ""
+    xml_text = ""
+    tags = []
+    in_narr = False
+    in_tags = False
+    x = 0
+    while x<len(lines):
+        line = lines[x]#.strip()
+        if len(line) == 0:
             x = x+1
-            index = index+1
-                
-        filename = args.ann_dir + "/" + rec_id + "_" + ann_name + ".xml"
-        print "writing " + filename
-        outfile = open(filename, 'w')
-        outfile.write('<?xml version="1.0" encoding="UTF-8" ?>\n<simpleTimeML>\n')
-        outfile.write('<TEXT><![CDATA[')
-        outfile.write(narr_text)
-        outfile.write(']]></TEXT>\n<TAGS>')
-        outfile.write(narr_tags)
-        outfile.write("\n</TAGS>\n</simpleTimeML>\n")
-        #outfile.write("</root>\n")
+            continue
+        # Get original narr text
+        if in_narr:
+            if cdata_end in line:
+                if len(line) > len(cdata_end):
+                    stuff = line[:-(len(cdata_end))]
+                    narr_text = narr_text + " " + stuff
+                #narr_text = narr_text.strip()
+                in_narr = False
+            else:
+                narr_text = narr_text + " " + line
+        # Process tags
+        elif in_tags:
+            if tag_end in line:
+                if len(line) > len(tag_end):
+                    stuff = line[:-len(tag_end)]
+                    tags.append(create_tag(stuff))
+                in_tags = False
+            else:
+                tags.append(create_tag(line))
+        # Find the start of the text
+        elif cdata_start in line:
+            in_narr = True
+            if len(line) > len(cdata_start):
+                if cdata_end in line: # Text is all on one line
+                    s = len(cdata_start)
+                    e = len(line) - len(cdata_end)
+                    narr_text = line[s:e]
+                    in_narr = False
+                else:
+                    stuff = line[len(cdata_start):]
+                    narr_text = narr_text + " " + stuff
+        elif tag_start in line:
+            in_tags = True
+            if len(line) > len(tag_start):
+                stuff = line[len(tag_start):]
+                tags.append(create_tag(stuff))
+        # Go to the next line        
+        x = x+1
 
+    # Sort tags by span start
+    tags.sort(key=lambda x: x.start)
+
+    # Construct xml narrative
+    i = 0
+    tlinks = []
+    for z in range(len(tags)):
+        tag = tags[z]
+        if tag.name == "TLINK":
+            tlinks.append(tag)
+        else:
+            if i < tag.start:
+                xml_text = xml_text + narr_text[i:tag.start]
+            xml_text = xml_text + str(tag)
+            i = tag.end
+    if i < len(narr_text):
+        xml_text = xml_text + narr_text[i:]
+    for t in tlinks:
+        xml_text = xml_text + " " + str(t)
+
+    return xml_text
+
+def create_tag(line):
+    timex = "<TIMEX3"
+    event = "<EVENT"
+    signal = "<SIGNAL"
+    tlink = "<TLINK"
+    sectime = "<SECTIME"
+    #attributes = ["polarity=", "relatedToTime"]
+
+    name = ""
+    tid = ""
+    start = ""
+    end = ""
+    text = ""
+    attributes = ""
+
+    print line
+
+    chunks = line.split(' ')
+
+    x = 0
+    
+    while x<len(chunks):
+        chunk = chunks[x]
+        if timex in chunk:
+            name = "TIMEX3"
+        elif event in chunk:
+            name = "EVENT"
+        elif signal in chunk:
+            name = "SIGNAL"
+        elif tlink in chunk:
+            name = "TLINK"
+        elif sectime in chunk:
+            name = "SECTIME"
+        elif "spans=" in chunk:
+            string = chunk[7:-1]
+            parts = string.split('~')
+            start = int(parts[0])
+            end = int(parts[1])
+            print "start: " + str(start) + ", end: " + str(end)
+        elif "start=" in chunk:
+            start = int(chunk[7:-1])
+        elif "end=" in chunk:
+            end = int(chunk[5:-1])
+        elif "text=" in chunk:
+            chunk = chunk[6:]
+            text = ""
+            while '"' not in chunk:
+                text = text + chunk + " "
+                x = x+1
+                chunk = chunks[x]
+            text = text + chunk[:-1]
+        elif "id=" in chunk:
+            tid = chunk[4:-1]
+        elif chunk != "/>":
+            attributes = attributes + " " + chunk
+        x = x+1
+
+    # TODO: process TLINKS?
+    print "name: " + name + ", tid: " + tid + ", text: " + text + ", atts: " + attributes
+    
+    tag = Tag(name, tid, start, end, text, attributes)
+    return tag
 
 if __name__ == "__main__":main()
--- annotation/xml_to_mae.py
+++ annotation/xml_to_mae.py
@@ -13,7 +13,7 @@ def main():
     args = argparser.parse_args()
 
     if not (args.infile and args.ann_dir):
-        print "usage: ./xml_to_mae_out.py --in [file_ann.xml] --dir [path/to/dir]"
+        print "usage: ./xml_to_mae.py --in [file_ann.xml] --dir [path/to/dir]"
         exit()
 
     inname = args.infile
@@ -31,94 +31,104 @@ def main():
         id_node = child.find("MG_ID")
         rec_id = id_node.text
         narr_text_node = child.find("narrative")
-        narr_text = narr_text_node.text
-        narr_node = child.find("narr_timeml_simple")
-        narr = etree.tostring(narr_node)
-        # Extract tags and spans
-        narr = narr[20:(len(narr)-21)]
-        print "narr: " + narr
-        x = 0
-        index = 0
+        narr_text = ""
         narr_tags = ""
-        while x < len(narr):
+        if narr_text_node is not None:
+            narr_text = narr_text_node.text
+        narr_node = child.find("narr_timeml_simple")
+        if narr_node is not None:
+            narr = etree.tostring(narr_node)
+            # Extract tags and spans
+            narr = narr[20:(len(narr)-21)]
+            print "narr: " + narr
+            x = 0
+            index = 0
+            narr_tags = ""
+            while x < len(narr):
             
-            char = narr[x]
-            if char == '<':
-                startspan = index
-                x = x+1
-                if len(narr) > x+5:
-                    tag = narr[x:x+6]
-                    print "found tag: " + tag
-                    idnum = ""
-                    # Fix longer tags
-                    if tag == "TIMEX3" or tag == "SIGNAL":
-                        x = x+1
-                    tag = tag.strip()
-                    # Skip space and id tag name
-                    x = x+11
-                    # Get id
-                    end = narr.index('"', x)
-                    idnum = narr[x:end]
-                    x = end
-                    # Create tag string
-                    restoftag = ""
-                    char = narr[x]
-                    while char != '>' and x < len(narr):
-                        x = x+1
-                        char = narr[x]
-                        if char != ">":
-                            restoftag = restoftag + char
-                    if tag != "TLINK":
-                        # Calcuate span
-                        text = ""
-                        # Skip initial space
-                        x = x+1
-                        if narr_text[index] == " ":
-                            index = index+1
-                        print "x: " + str(x) + " i: " + str(index)
-                        print "narr[x]: '" + str(narr[x]) + "' narr[i]: '" + str(narr_text[index]) + "'"
-                        while char != '<' and x < len(narr):
+                char = narr[x]
+                if char == '<':
+                    startspan = index
+                    x = x+1
+                    if len(narr) > x+5:
+                        tag = narr[x:x+6]
+                        print "found tag: " + tag
+                        idnum = ""
+                        # Fix longer tags
+                        if tag == "TIMEX3" or tag == "SIGNAL":
                             x = x+1
-                            index = index+1
-                            char = narr[x]
-                            text = text + char
-                        endspan = index-2
-                        index = index-2 # remove trailing space
-                        text = text[:-2]
-                        print "text from x: '" + text + "'"
-                        text = narr_text[startspan:endspan].strip()
-                        print "text from i: '" + text + "'"
-                        print "x: " + str(x) + " i: " + str(index)
-                        #narr_text = narr_text + text + " "
-                        narr_tags = narr_tags + '<' + tag + ' id="' + idnum + '" spans="' + str(startspan) + '~' + str(endspan) + '" text="' + text + '" ' + restoftag + "/>\n"
-                        # Skip over end tag
+                        tag = tag.strip()
+                        # Skip space and id tag name
+                        x = x+11
+                        # Get id
+                        end = narr.index('"', x)
+                        idnum = narr[x:end]
+                        x = end
+                        # Create tag string
+                        restoftag = ""
+                        char = narr[x]
                         while char != '>' and x < len(narr):
                             x = x+1
                             char = narr[x]
-                    else: # TLINK TODO: create TLINKs from event attributes
-                        from_start = restoftag.index('eventID') +9
-                        from_end = restoftag.index('"', from_start)
-                        from_id = restoftag[from_start:from_end]
-                        to_start = restoftag.index('relatedToTime') +15
-                        to_end = restoftag.index('"', to_start)
-                        to_id = restoftag[to_start:to_end]
-                        print "TLINK from: " + from_id + " to: " + to_id
-                        narr_tags = narr_tags + '<' + tag + ' id="' + idnum + '" from="' + from_id + '" to="' + to_id + '" ' + restoftag + ">\n"
-                    x = x+1 #Skip the space after the tag
-                    #char = narr[x]
-                    #if (char == " "):
-                    #    x = x+1
-                    #    char = narr[x]
-                    #    print "x: " + str(x) + " char: " + char
-            x = x+1
-            index = index+1
+                            if char != ">":
+                                restoftag = restoftag + char
+                        if tag != "TLINK" and index < len(narr_text):
+                            # Calcuate span
+                            text = ""
+                            # Skip initial space
+                            x = x+1
+                            #print "narr_text[i]: " + narr_text[index]
+                            if narr_text[index] == ' ':
+                                index = index+1
+                                print "skipped space"
+                            print "about to process tag"
+                            print "narr[x=" + str(x) + "]: '" + str(narr[x]) + "' narr[i=" + str(index) + "]: '" + str(narr_text[index]) + "'"
+                            while char != '<' and x < len(narr):
+                                x = x+1
+                                index = index+1
+                                char = narr[x]
+                                text = text + char
+                            endspan = index-2
+                            index = index-2 # remove trailing space
+                            text = text[:-2]
+                            print "text from x: '" + text + "'"
+                            text = narr_text[startspan:endspan].strip()
+                            print "text from i: '" + text + "'"
+                            print "x: " + str(x) + " i: " + str(index)
+                            #narr_text = narr_text + text + " "
+                            narr_tags = narr_tags + '<' + tag + ' id="' + idnum + '" spans="' + str(startspan) + '~' + str(endspan) + '" text="' + text + '" ' + restoftag + "/>\n"
+                            # Skip over end tag
+                            while char != '>' and x < len(narr):
+                                x = x+1
+                                char = narr[x]
+                            #x = x+1
+                        #else: # TLINK TODO: create TLINKs from event attributes
+                        #    from_start = restoftag.index('eventID') +9
+                        #    from_end = restoftag.index('"', from_start)
+                        #    from_id = restoftag[from_start:from_end]
+                        #    to_start = restoftag.index('relatedToTime') +15
+                        #    to_end = restoftag.index('"', to_start)
+                        #    to_id = restoftag[to_start:to_end]
+                        #    print "TLINK from: " + from_id + " to: " + to_id
+                        #    narr_tags = narr_tags + '<' + tag + ' id="' + idnum + '" from="' + from_id + '" to="' + to_id + '" ' + restoftag + ">\n"
+                        x = x+1 #Skip the space after the tag
+                        print "end of loop"
+                        if x < len(narr):
+                            char = narr[x]
+                            if char == ' ':
+                                x = x+1
+                                #char = narr[x]
+                            print "x=" + str(x) + " : " + char + ", i=" + str(index) + " : " + narr_text[index]
+                else:
+                    x = x+1
+                    index = index+1
                 
         filename = args.ann_dir + "/" + rec_id + "_" + ann_name + ".xml"
         print "writing " + filename
         outfile = open(filename, 'w')
         outfile.write('<?xml version="1.0" encoding="UTF-8" ?>\n<simpleTimeML>\n')
         outfile.write('<TEXT><![CDATA[')
-        outfile.write(narr_text)
+        outfile.write(narr_text.encode('utf-8'))
         outfile.write(']]></TEXT>\n<TAGS>')
         outfile.write(narr_tags)
         outfile.write("\n</TAGS>\n</simpleTimeML>\n")
