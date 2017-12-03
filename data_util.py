1ad0855e36559ff10cdc3f3692fb9e89c46ead51
Serena Jeblee <sjeblee@cs.toronto.edu>
2017-12-02 21:16:44 -0500

    Update data_util

--- data_util.py
+++ data_util.py
@@ -2,6 +2,37 @@
 # Util functions
 # @author sjeblee@cs.toronto.edu
 
+import subprocess
+
+def clean_file(filename):
+    # remove blank lines | remove extra spaces| remove leading and trailing spaces  | fix utf-8 chars
+    command = r"sed '/^\s*$/d' $file | sed -e 's/  */ /g' | sed -e 's/^ //g' | sed -e 's/ $//g' | sed -e 's/&amp;/and/g' | sed -e 's/&#13;/ /g' | sed -e 's/&#8217;/\'/g' | sed -e 's/&#8221;/\"/g' | sed -e 's/&#8220;/\"/g' | sed -e 's/&#65533;//g' | sed -e 's/&#175\7;//g'| sed -e 's/&#1770;/\'/g'"
+    # TODO
+    
+
+''' Remove blank lines, convert \n to space, remove double spaces, insert a line break before each record
+    filename: the file to fix (file will be overwritten)
+    rec_type: the type of record: adult, child, or neonate
+'''
+def fix_line_breaks(filename, rec_type):
+    tag = "<Adult_Anonymous>"
+    if rec_type == "child":
+        tag = "<Child_Anonymous>"
+    elif rec_type == "neonate":
+        tag = "<Neonate_Anonymous>"
+    sed_command = "s/" + tag + r"/\n" + tag + "/g"
+    sed_command2 = r"sed -e 's/<\/root>/\n<\/root>/g'"
+    #print "sed_command: " + sed_command
+    tr_command = "tr " + r"'\n' " + "' '"
+    #print "tr_command: " + tr_command
+    f = open("temp", 'wb')
+    command = "sed -e '/^\s$/d' " + filename + " | " + tr_command + " | sed -e 's/  / /g' | sed -e '" + sed_command + "'" + " | " + sed_command2
+    ps = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE)
+    output = ps.communicate()[0]
+    out = open(filename, 'w')
+    out.write(output)
+    out.close()
+
 ''' matrix: a list of dictionaries
     dict_keys: a list of the dictionary keys
     outfile: the file to write to
