2093b621b4c7b8bd25d26568c656fffcbc7f4d15
Serena Jeblee <sjeblee@cs.toronto.edu>
2017-12-07 15:13:18 -0500

    Add fix_arrows and stringify_children to data_util

--- data_util.py
+++ data_util.py
@@ -3,13 +3,28 @@
 # @author sjeblee@cs.toronto.edu
 
 from lxml import etree
+from lxml.etree import tostring
+from itertools import chain
 import subprocess
 
 def clean_file(filename):
     # remove blank lines | remove extra spaces| remove leading and trailing spaces  | fix utf-8 chars
     command = r"sed '/^\s*$/d' $file | sed -e 's/  */ /g' | sed -e 's/^ //g' | sed -e 's/ $//g' | sed -e 's/&amp;/and/g' | sed -e 's/&#13;/ /g' | sed -e 's/&#8217;/\'/g' | sed -e 's/&#8221;/\"/g' | sed -e 's/&#8220;/\"/g' | sed -e 's/&#65533;//g' | sed -e 's/&#175\7;//g'| sed -e 's/&#1770;/\'/g'"
     # TODO
-    
+
+''' Convert arrows in text to non-arrows (for xml processing)
+    filename: the file to fix (file will be overwritten)
+'''
+def fix_arrows(filename):
+    sed_command = r"sed -e 's/-->/to/g' " + filename + r" | sed -e 's/->/to/g' | sed -e 's/ < / lt /g' | sed -e 's/ > / gt /g'"
+    print "sed_command: " + sed_command
+    #f = open("temp", 'wb')
+    ps = subprocess.Popen(sed_command, shell=True, stdout=subprocess.PIPE)
+    output = ps.communicate()[0]
+    out = open(filename, 'w')
+    out.write(output)
+    out.close()
+
 
 ''' Remove blank lines, convert \n to space, remove double spaces, insert a line break before each record
     filename: the file to fix (file will be overwritten)
@@ -26,7 +41,7 @@ def fix_line_breaks(filename, rec_type):
     #print "sed_command: " + sed_command
     tr_command = "tr " + r"'\n' " + "' '"
     #print "tr_command: " + tr_command
-    f = open("temp", 'wb')
+    #f = open("temp", 'wb')
     command = "sed -e '/^\s$/d' " + filename + " | " + tr_command + " | sed -e 's/  / /g' | sed -e '" + sed_command + "'" + " | " + sed_command2
     ps = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE)
     output = ps.communicate()[0]
@@ -53,6 +68,14 @@ def remove_no_narrs(infile, outfile):
 
     print "Removed " + str(count) + " missing or empty narratives"
     tree.write(outfile)
+
+''' Get content of a tree node as a string
+    node: etree.Element
+'''
+def stringify_children(node):
+    parts = ([node.text] + list(chain(*([tostring(c)] for c in node.getchildren()))))
+    # filter removes possible Nones in texts and tails
+    return ''.join(filter(None, parts))
 
 ''' matrix: a list of dictionaries
     dict_keys: a list of the dictionary keys
