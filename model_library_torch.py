2cac896203cb5ebd3054931b1e9355f53c22682f
Serena Jeblee <sjeblee@cs.toronto.edu>
2018-05-04 17:27:53 -0400

    Update model_library_torch to python3

--- model_library_torch.py
+++ model_library_torch.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 # Neural network model functions in PyTorch
 # @author sjeblee@cs.toronto.edu
 
@@ -12,13 +12,28 @@ import torch.nn as nn
 from torch.autograd import Variable
 from torch import optim
 import torch.nn.functional as F
-import matplotlib.pyplot as plt
-import matplotlib.ticker as ticker
+#import matplotlib.pyplot as plt
+#import matplotlib.ticker as ticker
 
-import data_util
+import data_util3
 
 numpy.set_printoptions(threshold=numpy.inf)
 use_cuda = torch.cuda.is_available()
+torch.cuda.set_device(2)
+
+# Neural Network Model (1 hidden layer)
+class Net(nn.Module):
+    def __init__(self, input_size, hidden_size, num_classes):
+        super(Net, self).__init__()
+        self.fc1 = nn.Linear(input_size, hidden_size)
+        self.relu = nn.ReLU()
+        self.fc2 = nn.Linear(hidden_size, num_classes)
+
+    def forward(self, x):
+        out = self.fc1(x)
+        out = self.relu(out)
+        out = self.fc2(out)
+        return out
 
 # Models from PyTorch Tutorial:
 ######################################################################
@@ -40,14 +55,19 @@ class EncoderRNN(nn.Module):
         super(EncoderRNN, self).__init__()
         self.hidden_size = hidden_size
 
-        self.embedding = nn.Embedding(input_size, hidden_size)
-        self.gru = nn.GRU(hidden_size, hidden_size)
+        #print "embedding init size: " + str(input_size)
+        #self.embedding = nn.Embedding(input_size, hidden_size)
+        self.gru = nn.GRU(input_size, hidden_size)
 
     def forward(self, input, hidden):
-        print "forward: input: " + str(type(input))
-        embedded = self.embedding(input).view(1, 1, -1)
-        output = embedded
-        output, hidden = self.gru(output, hidden)
+        #print "forward: input: " + str(input.size())
+        #embedded1 = self.embedding(input)
+        embedded1 = input
+        #print "embedded1: " + str(embedded1.size())
+        embedded = embedded1.view(1, 1, -1)
+        #print "embedded: " + str(embedded.size())
+        #output = input
+        output, hidden = self.gru(embedded, hidden)
         return output, hidden
 
     def initHidden(self):
@@ -146,7 +166,7 @@ class AttnDecoderRNN(nn.Module):
         self.dropout_p = dropout_p
         self.max_length = max_length
 
-        self.embedding = nn.Embedding(self.output_size, self.hidden_size)
+        #self.embedding = nn.Embedding(self.output_size, self.hidden_size)
         self.attn = nn.Linear(self.hidden_size * 2, self.max_length)
         self.attn_combine = nn.Linear(self.hidden_size * 2, self.hidden_size)
         self.dropout = nn.Dropout(self.dropout_p)
@@ -154,10 +174,15 @@ class AttnDecoderRNN(nn.Module):
         self.out = nn.Linear(self.hidden_size, self.output_size)
 
     def forward(self, input, hidden, encoder_outputs):
-        embedded = self.embedding(input).view(1, 1, -1)
+        #print "encoder_outputs: " + str(encoder_outputs.size())
+        #print "adecoder input: " + str(input.size())
+        #embedded = self.embedding(input).view(1, 1, -1)
+        embedded = input.view(1, 1, -1)
+        #print "embedded: " + str(embedded.size())
         embedded = self.dropout(embedded)
 
         attn_weights = F.softmax(self.attn(torch.cat((embedded[0], hidden[0]), 1)), dim=1)
+        #print "attn_weights: " + str(attn_weights.size())
         attn_applied = torch.bmm(attn_weights.unsqueeze(0), encoder_outputs.unsqueeze(0))
 
         output = torch.cat((embedded[0], attn_applied[0]), 1)
@@ -188,45 +213,137 @@ class AttnDecoderRNN(nn.Module):
     returns: the model and the modified X and Y arrays
 '''
 def nn_model(X, Y, num_nodes, act, num_epochs=10):
-    X = numpy.asarray(X)
-    Y = to_categorical(Y)
-    #print "X.shape: " + str(X.shape)
-    #print "Y.shape: " + str(Y.shape)
+    print("use_cuda", str(use_cuda))
+    X = torch.Tensor(X)
+    Y = torch.Tensor(Y).long()
+    if use_cuda:
+        X = X.cuda()
+        Y = Y.cuda()
+    print("X: ", str(X.size()))
+    print("Y: ", str(Y.size()))
 
-    #print "neural network: nodes: " + str(num_nodes)
-    #nn = Sequential([Dense(num_nodes, input_dim=X.shape[-1]),
-    #                Activation(act),
-                    #Dense(num_nodes, input_dim=num_feats),
-                    #Activation(activation),
-   #                 Dense(Y.shape[1]),
-    #                Activation('softmax'),])
-        
-    #nn.compile(optimizer='rmsprop', loss='categorical_crossentropy', metrics=['accuracy'])
-    #nn.fit(X, Y, epochs=num_epochs)
-    #nn.summary()
-    #return nn, X, Y
+    # Hyper Parameters
+    input_dim = X.size()[-1]
+    num_examples = X.size()[0]
+    num_classes = Y.size()[-1]
+    batch_size = 100
+    learning_rate = 0.001
+
+    print("neural network: nodes: ", str(num_nodes))
+    net = Net(input_dim, num_nodes, num_classes)
+
+    if use_cuda:
+        net = net.cuda()
+
+    # Loss and Optimizer
+    criterion = nn.CrossEntropyLoss()
+    optimizer = torch.optim.Adam(net.parameters(), lr=learning_rate)
+
+    # Train the Model
+    for epoch in range(num_epochs):
+        print("epoch", str(epoch))
+        i = 0
+        while (i+batch_size) < num_examples:
+            if i%100000 == 0:
+                print("batch i=", str(i))
+            # Convert torch tensor to Variable
+            samples = Variable(X[i:i+batch_size])
+            labels = Variable(Y[i:i+batch_size])
+
+            # Forward + Backward + Optimize
+            optimizer.zero_grad()  # zero the gradient buffer
+            outputs = net(samples)
+            loss = criterion(outputs, labels)
+            loss.backward()
+            optimizer.step()
+
+            if (i+1) % 10 == 0:
+                print ('Epoch [%d/%d], Step [%d/%d], Loss: %.4f' %(epoch+1, num_epochs, i+1, len(train_dataset)//batch_size, loss.data[0]))
+            i = i+batch_size
+
+    del X
+    del Y
+    if use_cuda:
+        torch.cuda.empty_cache()
+    return net
+
+def test_nn(net, testX):
+    # Test the Model
+    #testX = testX.astype('float')
+    pred = []
+    batch_size = 100
+    i = 0
+    length = len(testX) #testX.shape[0]
+    while i < length:
+        if i%100000 == 0:
+            print("test batch", str(i))
+        if (i+batch_size) > length:
+            batch_size = length-i
+        if use_cuda:
+            sample_tensor = torch.cuda.FloatTensor(testX[i:i+batch_size])
+        else:
+            sample_tensor = torch.FloatTensor(testX[i:i+batch_size])
+        samples = Variable(sample_tensor)
+        outputs = net(samples)
+        _, predicted = torch.max(outputs.data, 1)
+        pred = pred + predicted.tolist()
+        del sample_tensor
+        i = i+batch_size
+    return pred
 
 ''' Creates and trains a recurrent neural network model. Supports SimpleRNN, LSTM, and GRU
     X: a list of training data
     Y: a list of training labels
 '''
 def rnn_model(X, Y, num_nodes, activation='sigmoid', modelname='lstm', dropout=0.1, X2=[], pretrainX=[], pretrainY=[], pretrainX2=[], initial_states=None, num_epochs=15):
-    #Y = to_categorical(Y)
-    X = numpy.asarray(X)
-    print "train X shape: " + str(X.shape)
-    embedding_size = X.shape[-1]
-    max_seq_len = X.shape[1]
-    inputs = []
-    input_arrays = [X]
-    hybrid = False
-    pretrain = False
-    if len(pretrainX) > 0 and len(pretrainY) > 0:
-        pretrain = True
-        print "Using pretraining"
+    print("model: ", modelname, " nodes: ", str(num_nodes), " embedding: ", str(embedding_size), " max_seq_len: ", str(max_seq_len))
 
-    print "model: " + modelname + " nodes: " + str(num_nodes) + " embedding: " + str(embedding_size) + " max_seq_len: " + str(max_seq_len)
+    n_iters = len(X)
+    print("epochs: ", str(num_epochs), " iters: ", str(n_iters))
+    if use_cuda:
+        print("using cuda")
+        X = torch.cuda.FloatTensor(X)
+        Y = torch.cuda.FloatTensor(Y)
+    else:
+        X = torch.FloatTensor(X)
+        Y = torch.FloatTensor(Y)
+    print("X: ", str(X.size()))
+    print("Y: ", str(Y.size()))
+    hidden_size = num_nodes
+    input_dim = X.size()[-1]
+    max_length = Y.size()[1]
+    output_dim = Y.size()[-1]
+    print("input_dim: ", str(input_dim))
+    print("max_length: ", str(max_length))
+    print("output_dim: ", str(output_dim))
+    rnn = AttnDecoderRNN(hidden_size, output_dim, dropout_p=0.1, max_length=X.size()[1])
 
-    print "NOT IMPLEMENTED"
+    if use_cuda:
+        rnn = decoder1.cuda()
+
+    start = time.time()
+    plot_losses = []
+    print_loss_total = 0  # Reset every print_every
+    plot_loss_total = 0  # Reset every plot_every
+
+    encoder_optimizer = optim.SGD(encoder1.parameters(), lr=learning_rate)
+    decoder_optimizer = optim.SGD(decoder1.parameters(), lr=learning_rate)
+    #training_pairs = [variablesFromPair(random.choice(pairs)) for i in range(n_iters)]
+    training_pairs = zip(X, Y)
+    if loss_function == "cosine":
+        criterion = nn.CosineEmbeddingLoss()
+    else:
+        print("ERROR: need to add loss type")
+
+    for ep in range(num_epochs):
+        print('epoch ', str(ep))
+        for iter in range(1, n_iters + 1):
+            print("iter: ", str(iter))
+            training_pair = training_pairs[iter-1]
+            input_variable = Variable(training_pair[0])
+            target_variable = Variable(training_pair[1])
+
+    print("NOT IMPLEMENTED")
 
 
 ''' Create and train a CNN model
@@ -236,18 +353,18 @@ def rnn_model(X, Y, num_nodes, activation='sigmoid', modelname='lstm', dropout=0
 '''
 def cnn_model(X, Y, act=None, windows=[1,2,3,4,5], X2=[], num_epochs=10, loss_func='categorical_crossentropy'):
     #Y = to_categorical(Y)
-    X = numpy.asarray(X)
-    embedding_size = X.shape[-1]
-    max_seq_len = X.shape[1]
-    print "train X shape: " + str(X.shape)
-    print "max_seq_len: " + str(max_seq_len)
-    inputs = []
-    input_arrays = [X]
-    hybrid = False
+    #X = numpy.asarray(X)
+    #embedding_size = X.shape[-1]
+    #max_seq_len = X.shape[1]
+    #print "train X shape: " + str(X.shape)
+    #print "max_seq_len: " + str(max_seq_len)
+    #inputs = []
+    #input_arrays = [X]
+    #hybrid = False
 
-    print "NOT IMPLEMENTED"
+    print("NOT IMPLEMENTED")
 
-def train_encoder_decoder(input_variable, target_variable, encoder, decoder, encoder_optimizer, decoder_optimizer, criterion, max_length):
+def train_encoder_decoder(input_variable, target_variable, encoder, decoder, encoder_optimizer, decoder_optimizer, loss_func, max_length):
     # Parameters
     teacher_forcing_ratio = 0.5
     loss = 0
@@ -259,17 +376,20 @@ def train_encoder_decoder(input_variable, target_variable, encoder, decoder, enc
 
     input_length = input_variable.size()[0]
     target_length = target_variable.size()[0]
-    print "input_length: " + str(input_length) + ", target_length: " + str(target_length)
+    #print "input_length: " + str(input_length) + ", target_length: " + str(target_length)
     input_dim = input_variable.size()[-1]
     output_dim = target_variable.size()[-1]
-    start_token = torch.zeros(input_dim)
-    end_token = torch.ones(output_dim).long()
+    start_token = torch.zeros(output_dim)
+    end_token = Variable(torch.zeros(output_dim))
+    loss_target = Variable(torch.ones(1))
 
-    encoder_outputs = Variable(torch.zeros(max_length, encoder.hidden_size))
+    #print "start token size: " + str(start_token.size())
+
+    encoder_outputs = Variable(torch.zeros(input_length, encoder.hidden_size))
     encoder_outputs = encoder_outputs.cuda() if use_cuda else encoder_outputs
     for ei in range(input_length):
-        print "ei: " + str(ei)
-        print "encoder_hidden: " + str(type(encoder_hidden.data))
+        #print "ei: " + str(ei)
+        #print "encoder_hidden: " + str(encoder_hidden.data.size())
         encoder_output, encoder_hidden = encoder(input_variable[ei], encoder_hidden)
         encoder_outputs[ei] = encoder_output[0][0]
 
@@ -282,23 +402,37 @@ def train_encoder_decoder(input_variable, target_variable, encoder, decoder, enc
     if use_teacher_forcing:
         # Teacher forcing: Feed the target as the next input
         for di in range(target_length):
+            #print "di (tf): " + str(di)
             decoder_output, decoder_hidden, decoder_attention = decoder(decoder_input, decoder_hidden, encoder_outputs)
-            loss += criterion(decoder_output, target_variable[di])
+            target_var = target_variable[di].view([1,-1])
+            #print "- decoder_ouput: " + str(decoder_output.size())
+            #print "- target_variable[di]: " + str(target_var.size())
+            loss += loss_func(decoder_output, target_var, loss_target)
             decoder_input = target_variable[di]  # Teacher forcing
 
     else:
         # Without teacher forcing: use its own predictions as the next input
         for di in range(target_length):
+            #print "di: "
             decoder_output, decoder_hidden, decoder_attention = decoder(decoder_input, decoder_hidden, encoder_outputs)
-            topv, topi = decoder_output.data.topk(1)
-            ni = topi[0][0]
+            #print "decoder_output: " + str(decoder_output)
+            #topv, topi = decoder_output.data.topk(1)
+            #ni = topi[0][0]
+            #print "ni: " + str(type(ni)) + " : " + str(ni)
 
-            decoder_input = Variable(torch.FloatTensor([[ni]]))
+            #decoder_input = Variable(torch.FloatTensor([[ni]]))
+            decoder_input = decoder_output
             decoder_input = decoder_input.cuda() if use_cuda else decoder_input
 
-            loss += criterion(decoder_output, target_variable[di])
-            if ni == end_token:
+            target_var = target_variable[di].view([1,-1])
+            #print "- decoder_ouput: " + str(decoder_output.size())
+            #print "- target_variable[di]: " + str(target_var.size())
+
+            loss += loss_func(decoder_output, target_var, loss_target)
+            if torch.equal(decoder_output, end_token):
                 break
+
+    print("loss: ", str(loss))
 
     loss.backward()
     encoder_optimizer.step()
@@ -306,23 +440,28 @@ def train_encoder_decoder(input_variable, target_variable, encoder, decoder, enc
 
     return loss.data[0] / target_length
 
-def encoder_decoder_model(X, Y, num_nodes, print_every=1000, plot_every=100, learning_rate=0.01, num_epochs=10, loss_function=nn.CosineEmbeddingLoss):
-    print "Encoder-decoder model: " + str(num_nodes)
+def encoder_decoder_model(X, Y, num_nodes, print_every=1000, plot_every=100, learning_rate=0.01, num_epochs=10, loss_function="cosine"):
+    print("Encoder-decoder model: ", str(num_nodes))
     n_iters = len(X)
-    print "epochs: " + str(num_epochs) + " iters: " + str(n_iters)
+    print("epochs: ", str(num_epochs), " iters: ", str(n_iters))
     if use_cuda:
+        print("using cuda")
         X = torch.cuda.FloatTensor(X)
         Y = torch.cuda.FloatTensor(Y)
     else:
         X = torch.FloatTensor(X)
         Y = torch.FloatTensor(Y)
-    print "X: " + str(X.size())
-    print "Y: " + str(Y.size())
+    print("X: ", str(X.size()))
+    print("Y: ", str(Y.size()))
     hidden_size = num_nodes
+    input_dim = X.size()[-1]
     max_length = Y.size()[1]
     output_dim = Y.size()[-1]
-    encoder1 = EncoderRNN(X.size()[-1], hidden_size)
-    decoder1 = AttnDecoderRNN(hidden_size, output_dim, dropout_p=0.1)
+    print("input_dim: " + str(input_dim))
+    print("max_length: " + str(max_length))
+    print("output_dim: " + str(output_dim))
+    encoder1 = EncoderRNN(input_dim, hidden_size)
+    decoder1 = AttnDecoderRNN(hidden_size, output_dim, dropout_p=0.1, max_length=X.size()[1])
 
 
     if use_cuda:
@@ -338,28 +477,35 @@ def encoder_decoder_model(X, Y, num_nodes, print_every=1000, plot_every=100, lea
     decoder_optimizer = optim.SGD(decoder1.parameters(), lr=learning_rate)
     #training_pairs = [variablesFromPair(random.choice(pairs)) for i in range(n_iters)]
     training_pairs = zip(X, Y)
-    criterion = loss_function
+    if loss_function == "cosine":
+        criterion = nn.CosineEmbeddingLoss()
+    else:
+        print("ERROR: need to add loss type")
 
     for ep in range(num_epochs):
-        print 'epoch ' + str(ep)
+        print('epoch ', str(ep))
         for iter in range(1, n_iters + 1):
+            print("iter: ", str(iter))
             training_pair = training_pairs[iter-1]
-            input_variable = training_pair[0]
-            target_variable = training_pair[1]
+            input_variable = Variable(training_pair[0])
+            target_variable = Variable(training_pair[1])
+
+            #print "input var: " + str(input_variable.data.size())
+            #print "target var: " + str(target_variable.data.size())
 
             loss = train_encoder_decoder(input_variable, target_variable, encoder1, decoder1, encoder_optimizer, decoder_optimizer, criterion, max_length)
             print_loss_total += loss
             plot_loss_total += loss
 
-            if iter % print_every == 0:
-                print_loss_avg = print_loss_total / print_every
-                print_loss_total = 0
-                print('%s (%d %d%%) %.4f' % (timeSince(start, iter / n_iters), iter, iter / n_iters * 100, print_loss_avg))
+            #if iter % print_every == 0:
+            #    print_loss_avg = print_loss_total / print_every
+            #    print_loss_total = 0
+            #    print('%s (%d %d%%) %.4f' % (timeSince(start, float(iter / n_iters)), iter, float(iter / n_iters) * 100, print_loss_avg))
 
-            if iter % plot_every == 0:
-                plot_loss_avg = plot_loss_total / plot_every
-                plot_losses.append(plot_loss_avg)
-                plot_loss_total = 0
+            #if iter % plot_every == 0:
+            #    plot_loss_avg = plot_loss_total / plot_every
+            #    plot_losses.append(plot_loss_avg)
+            #    plot_loss_total = 0
 
     return encoder1, decoder1, output_dim
 
@@ -433,24 +579,25 @@ def timeSince(since, percent):
     return '%s (- %s)' % (asMinutes(s), asMinutes(rs))
 
 def showAttention(input_sentence, output_words, attentions):
+    print("TODO")
     # Set up figure with colorbar
-    fig = plt.figure()
-    ax = fig.add_subplot(111)
-    cax = ax.matshow(attentions.numpy(), cmap='bone')
-    fig.colorbar(cax)
+    #fig = plt.figure()
+    #ax = fig.add_subplot(111)
+    #cax = ax.matshow(attentions.numpy(), cmap='bone')
+    #fig.colorbar(cax)
 
     # Set up axes
-    ax.set_xticklabels([''] + input_sentence.split(' ') + ['<EOS>'], rotation=90)
-    ax.set_yticklabels([''] + output_words)
+    #ax.set_xticklabels([''] + input_sentence.split(' ') + ['<EOS>'], rotation=90)
+    #ax.set_yticklabels([''] + output_words)
 
     # Show label at every tick
-    ax.xaxis.set_major_locator(ticker.MultipleLocator(1))
-    ax.yaxis.set_major_locator(ticker.MultipleLocator(1))
+    #ax.xaxis.set_major_locator(ticker.MultipleLocator(1))
+    #ax.yaxis.set_major_locator(ticker.MultipleLocator(1))
 
-    plt.show()
+    #plt.show()
 
 def evaluateAndShowAttention(input_sentence, encoder, attn_decoder):
-    output_words, attentions = evaluate(encoder, attn_decoder, input_sentence)
+    output_words, attentions = decode(encoder, attn_decoder, input_sentence, 10, 100)
     print('input =', input_sentence)
     print('output =', ' '.join(output_words))
     showAttention(input_sentence, output_words, attentions)
