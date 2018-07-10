95e476356d4406f6a0131aaebd1dffa2c829c432
Serena Jeblee <sjeblee@gmail.com>
2018-07-10 18:45:44 -0400

    Update pipeline.sh

--- pipeline.sh
+++ pipeline.sh
@@ -2,11 +2,11 @@
 # Script to run all the steps of the Verbal Autopsy pipeline
 
 # Directories for data input and output: SET THESE
-prefix="../../data"   # File output prefix
-dataloc="../../data/datasets"  # Location of data files (i.e. parent of mds+rct folder
+prefix="/u/sjeblee/research/va/data"   # File output prefix
+dataloc="/u/sjeblee/research/va/data/datasets"  # Location of data files (i.e. parent of mds+rct folder
 
-name="va_nn_counts"   # Give the output folder a name, will be created in $prefix
-modelname="nn"        # Model type: one of: nn cnn svm rnn gru
+name="va_nb_test"   # Give the output folder a name, will be created in $prefix
+modelname="nb"        # Model type: one of: nn cnn svm rnn gru
 prep="spell"          # Preprocessings: any comma-separated list of: none spell stem, kwc
 feats="narr_count"    # Features, comma-separated list of: narr_count narr_vec narr_symp dem narr_tfidf lda
 fname="narrc"         # Give the featureset a name
@@ -20,7 +20,7 @@ vecfile="/u/sjeblee/research/va/data/datasets/mds+rct/narr+ice+medhelp.vectors.1
 mkdir -p $prefix/$name
 
 # DEV
-python pipeline.py --name $name --model $modelname --dataset $dataset --modelname ${modelname}_${trainset}_${fname}_${labels} --train $trainset --dev $devset --preprocess $prep --features $feats --featurename $fname --labels $labels --vectors $vecfile --prefix $prefix --dataloc $dataloc | tee ../../data/$name/out-$devset-$modelname-$fname.txt
+python pipeline.py --name $name --model $modelname --dataset $dataset --modelname ${modelname}_${trainset}_${fname}_${labels} --train $trainset --dev $devset --preprocess $prep --features $feats --featurename $fname --labels $labels --vectors $vecfile --prefix $prefix --dataloc $dataloc | tee $prefix/data/$name/out-$devset-$modelname-$fname.txt
 
 # HYPEROPT
 #python pipeline.py --name "rf_hyperopt" --model "rf" --modelname "rf_h" --train "all" --dev "all" --features "type,narr_count" --ex "hyperopt" | tee ../../data/rf_hyperopt/out-hyperopt.txt
