a45fbe98e79c8e32ceed418c1397f8e417332cd0
Serena Jeblee <sjeblee@gmail.com>
2018-11-23 11:56:04 -0500

    Update the cleanfile script

--- tools/cleanfile.sh
+++ tools/cleanfile.sh
@@ -4,4 +4,4 @@ file=$1
 outfile=$2
 
 # remove blank lines | remove extra spaces| remove leading and trailing spaces  | fix utf-8 chars
-sed '/^\s*$/d' $file | sed -e 's/  */ /g' | sed -e 's/^ //g' | sed -e 's/ $//g' | sed -e 's/&amp;/and/g' | sed -e 's/&#13;/ /g' | sed -e "s/&#8217;/\'/g" | sed -e 's/&#8221;/"/g' | sed -e 's/&#8220;/"/g' | sed -e 's/&#65533;//g' | sed -e 's/&#1757;//g'| sed -e "s/&#1770;/\'/g" > $outfile
+sed '/^\s*$/d' $file | sed -e 's/  */ /g' | sed -e 's/^ //g' | sed -e 's/ $//g' | sed -e 's/&amp;/and/g' | sed -e 's/&#13;/ /g' | sed -e "s/&#8217;/\'/g" | sed -e 's/&#8221;/"/g' | sed -e 's/&#8220;/"/g' | sed -e 's/&#65533;//g' | sed -e 's/&#1757;//g'| sed -e "s/&#1770;/\'/g" | sed -e "s/_x000D_/ /g" | tr '\n' ' ' | tr '\r' ' ' | sed -e 's/^M/ /g' | sed -e 's/<Adult_Anonymous>/\n<Adult_Anonymous>/g' > $outfile
