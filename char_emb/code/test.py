d0ea1746f38f7ede5f141258f041e4228c74a86a
Zhaodong Yan <yanzhaod@hal9000.ais.sandbox>
2018-12-21 00:35:08 -0500

    this test file at least got one very good results, with f1_score equal to arounf 91% on child set

--- char_emb/code/test.py
+++ char_emb/code/test.py
@@ -21,25 +21,40 @@ import time
 import math
 from sklearn.metrics import f1_score
 import numpy as np
+from random import shuffle
 
-# In[3]:
 
 ###########################################################
+#**********************Parameters************************
+#cuda -> the selected gpu
+#input_train -> (string) the path of the input training file
+#out_model_filename -> (string) the stored net file that can be reloaded lator
+#out_text_filename -> (string) the output text file showing some relevant info
+#out_results_filename -> (string) the output file for buiding the confusion matrix 
+#n_hidden -> (int) hidden size of the neural net, usually 64 or 128
+#emb_size -> (int) embedding size
+
 #**********************Variables************************
-#data  ->   (dictionary), key=(string) cghr category; value=(string) narrative text 
+#data  ->   (dictionary), key=(string) cghr category; value=(tuple) ((string) MG_ID, (string)narrative text) 
 #vocab  ->  (string list), contains each of the letters
 #n_letters -> (int), number of letters
-
 ###########################################################
 
-
-
+# In[3]:
+cuda = torch.device("cuda:1")
 data={}         
 all_categories = []
 input_train = '/u/yanzhaod/data/va/mds+rct/train_child_cat.xml'
-out_model_filename = "./output/model_child_gru_test_64.pt"
+out_model_filename = "./output/model_child_gru_64.pt"
 out_text_filename = "output/out_child_gru_test_64.txt"
-out_results_filename = 'output/out_child_gru_test_results.txt'
+out_results_filename = 'output/out_child_results.txt'
+
+# Hidden size
+n_hidden = 128            
+
+# Embedding size
+emb_size = 20
+
 tree = etree.parse(input_train)
 for e in tree.iter("cghr_cat"):
         text = e.text.lower()
@@ -54,59 +69,68 @@ for child in root:
     narrative = child.find('narrative')
     cghr_cat = child.find('cghr_cat')
     try:
-        data[cghr_cat.text].append((MG_ID.text,narrative.text.lower()))
+        text = narrative.text.lower()
+        text = re.sub('[^a-z0-9\s]','',text)           #Note:this steps removes all punctionations and symbols in text, which is optional
+        data[cghr_cat.text].append((MG_ID.text,text))
     except AttributeError:
         continue
+        
+        
+# In[15]:  
 # for e in child.iter("MG_ID","narrative","cghr_cat"):
-#     print(e
+#     print(e)
 print("keys of data:")
 print(data.keys()[0])
 print("values of data:")
-print(data.values()[0][0])       
-
-
+print(data.values()[0][2])  
+#print(data.values()[0][4]) 
+#print(data.values()[0][28])      
+print('all categories: '+str(all_categories))
 n_categories= len(all_categories)
-
 n_iters = 0
 
 for k,v in data.iteritems():
     n_iters += len(v)
 print("size of the narratives: %d" %n_iters)
-# In[78]:
 
+# In[78]:
 all_text = ''
 for v in data.itervalues():
     l = [v[i][1] for i in range(len(v))]
     all_text = all_text + u"-".join(l)
 
-vocab = set(all_text)
+vocab = list(set(all_text))
+
 print("vocab: " +str(vocab))
 n_letters = len(vocab)
 
 def letterToIndex(letter):
-    return list(vocab).index(letter)
+    return vocab.index(letter)
 
 def letterToTensor(letter):
-    tensor = torch.zeros(1, n_letters)
+    tensor = torch.zeros([1, n_letters],device=cuda)
     tensor[0][letterToIndex(letter)] = 1
     return tensor
 
 # Turn a line into a <line_length x 1 x n_letters>,
 # or an array of one-hot letter vectors
 def lineToTensor(narrative):
-    tensor = torch.zeros(len(narrative),1)
+    tensor = torch.zeros([len(narrative),1],device=cuda)
     for li, letter in enumerate(narrative):
         tensor[li][0] = letterToIndex(letter)
     return tensor
-#
+
 print("a sample tensor of letter 'a':")
 print(letterToTensor('a'))
 narr = data[data.keys()[0]][1][1]
 print(data[data.keys()[0]][1])
 print("the size of a sample input (narr): ")
 input = lineToTensor(narr)
+input=input.to(cuda)
 print(input.size())
 
+for k in data:
+    print("key: " + str(k) + "; size: " +str(len(data[k]))) 
 # In[79]:
 
 import torch.nn as nn
@@ -125,19 +149,20 @@ class GRU(nn.Module):
         output,hidden = self.gru(input,hidden)
         output = self.linear(output[-1])
         output = self.softmax(output)
+        
         return output, hidden
         
     def initHidden(self):
-        return torch.zeros(1, self.hidden_size)
+        return torch.zeros([1, self.hidden_size],device=cuda)
 
-n_hidden = 64
-emb_size = 15
+
 gru = GRU(n_letters, n_hidden, n_categories,emb_size)
+gru.to(cuda)
+
+
 
 # In[104]:
-
-
-hidden = torch.zeros(1,1,n_hidden)
+hidden = torch.zeros([1,1,n_hidden],device=cuda)
 
 output, hn = gru(input,None)
 print("the sample output and output size: ")
@@ -152,11 +177,11 @@ def categoryFromOutput(output):
     #print(top_n)
     #print(top_i.size())
     category_i = top_i[0].item()
+    #print(category_i)
     return all_categories[category_i], category_i
 
 print("category from output:")
 print(categoryFromOutput(output))
-
 
 # In[109]:
 
@@ -167,12 +192,13 @@ def randomChoice(l):
 def randomTrainingExample():
     category = randomChoice(all_categories)
     line = randomChoice(data[category][1])
-    category_tensor = torch.tensor([all_categories.index(category)], dtype=torch.long)
+    category_tensor = torch.tensor([all_categories.index(category)], dtype=torch.long,device=cuda)
     line_tensor = lineToTensor(line)
     return category, line, category_tensor, line_tensor
 def getTensors(category,line):
-    category_tensor = torch.tensor([all_categories.index(category)], dtype=torch.long)
+    category_tensor = torch.tensor([all_categories.index(category)], dtype=torch.long,device=cuda)
     line_tensor = lineToTensor(line)
+    category_tensor = category_tensor.to(cuda)
     return category, line, category_tensor, line_tensor
     
 print("sample category,line,category_tensor from the randomTraningExample")
@@ -183,7 +209,7 @@ print('category =', category,category_tensor, '/ line =', line)
 
 # In[110]:
 
-learning_rate = 0.01
+learning_rate = 0.001
 criterion = nn.NLLLoss()
 optimizer = torch.optim.Adam(gru.parameters(),lr=learning_rate)
 
@@ -226,34 +252,40 @@ def train_iter():
     
     print_every = 100
     plot_every = 500
-    epochs = 7
+    epochs = 20
     # Keep track of losses for plotting
     current_loss = 0
     all_losses = []
     start = time.time()
     iter = 0
     #for iter in range(1, n_iters + 1):
-
+    l = []
+    for k in data:
+        v = data[k]
+        for i in range(len(v)):
+            l.append((k,v[i]))
+    shuffle(l)
+    print(len(l))
     for e in range(epochs):
-        for k,v in data.iteritems():
-            for i in range(len(v)):
-                iter += 1
-                category, line, category_tensor, line_tensor = getTensors(k,v[i][1])
-                output, loss = train(category_tensor, line_tensor)
-                current_loss += loss
 
+        for e in l:
+            k,v = e[0],e[1]
+            iter += 1
+            category, line, category_tensor, line_tensor = getTensors(k,v[1])
+            output, loss = train(category_tensor, line_tensor)
+            current_loss += loss
+            guess, guess_i = categoryFromOutput(output)
+            #print('guess: %s;   category:  %s' %(guess, category))    
+            # Print iter number, loss, name and guess
+            if iter % print_every == 0:
                 guess, guess_i = categoryFromOutput(output)
-                #print('guess: %s;   category:  %s' %(guess, category))    
-                # Print iter number, loss, name and guess
-                if iter % print_every == 0:
-                    guess, guess_i = categoryFromOutput(output)
-                    correct = '✓' if guess == category else '✗ (%s)' % category
-                    print('%d %d%% (%s) %.4f %s / %s %s' % (iter, iter / n_iters/epochs*100, timeSince(start), loss, line, guess, correct))
-        
-                # Add current loss avg to list of losses
-                if iter % plot_every == 0:
-                    all_losses.append(current_loss / plot_every)
-                    current_loss = 0
+                correct = '✓' if guess == category else '✗ (%s)' % category
+                print('%d %d%% (%s) %.4f %s / %s %s' % (iter, iter / n_iters/epochs*100, timeSince(start), loss, line, guess, correct))
+    
+            # Add current loss avg to list of losses
+            if iter % plot_every == 0:
+                all_losses.append(current_loss / plot_every)
+                current_loss = 0
     save()
     writeToFile("losses: " + str(all_losses),out_text_filename)
     return gru
@@ -275,10 +307,12 @@ def testTrainSet(model):
             output,hidden = gru(line_tensor,None)
             guess, guess_i = categoryFromOutput(output)
             result.append({'Correct_ICD':category,'Predicted_ICD':guess,'MG_ID':MG_ID})
-            #print(guess,category)
-    #f1score = f1_score(cat_true,cat_pred,average="weighted")
-    #print(f1score)
-    #writeToFile("f1score: " + str(f1score),out_text_filename)
+            print(category,guess)         #uncomment this line for detailed label/prediction pairs
+            cat_pred.append(guess)
+            cat_true.append(category)
+    f1score = f1_score(cat_true,cat_pred,average="weighted")
+    print(f1score)
+    writeToFile("f1score: " + str(f1score),out_text_filename)
     for i in range(len(result)):
         result[i] = str(result[i])
     writeToFile('\n'.join(result),out_results_filename)
