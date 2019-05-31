143b538d505b214bb33f02d3d0a08dd5dcbfc1b7
Serena Jeblee <sjeblee@gmail.com>
2019-05-31 17:28:03 -0400

    Convert addcategory.py to python3

--- addcategory.py
+++ addcategory.py
@@ -1,10 +1,9 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 # -*- coding: utf-8 -*-
 # Add the general COD category to the xml file
 
 from lxml import etree
 import argparse
-import string
 
 import data_util
 
@@ -17,7 +16,7 @@ def main():
     args = argparser.parse_args()
 
     if not (args.infile and args.outfile and args.mapfile):
-        print "usage: ./addcategory.py --in [file.xml] --out [outfile.xml] --map [map.csv] --label [ICD_cat]"
+        print("usage: ./addcategory.py --in [file.xml] --out [outfile.xml] --map [map.csv] --label [ICD_cat]")
         exit()
 
     label = "ICD_cat"
@@ -33,12 +32,12 @@ def main():
     removed = 0
     removed_unk = 0
     notfound = []
-    
+
     for child in root:
         id_node = child.find("MG_ID")
         rec_id = id_node.text
-        print "ID: " + rec_id
-        icd = "R99"
+        print('ID:', rec_id)
+        icd = 'R99'
         # Skip if record already has an ICD cat
         cat_node = child.find(label)
         overwrite = True
@@ -47,15 +46,15 @@ def main():
                 child.remove(cat_node)
             node = child.find("Final_code")
             icd = None
-            if node != None and node.text != None:
+            if node is not None and node.text is not None:
                 icd = node.text.upper()
             if icd is None or icd == "NULL" or icd == "NR" or icd == "":
-                print "Removing record!"
+                print('Removing record!')
                 root.remove(child)
                 removed = removed + 1
                 continue
             else:
-                print "ICD: " + icd
+                print('ICD:', icd)
             icdcat = etree.Element(label)
             if icd in icdmap:
                 icdcat.text = icdmap[icd]
@@ -63,18 +62,19 @@ def main():
                 if label == "ICD_cat_neo":
                     icdcat.text = "N5"
                 else:
-                    print "ICD not found: " + icd
+                    print('ICD not found:', icd)
                     notfound.append(icd)
                     root.remove(child)
                     removed_unk = removed_unk + 1
                     continue
             child.append(icdcat)
-        
+
     # write the xml to file
     tree.write(args.outfile)
 
-    print "Removed " + str(removed) + " records with NULL or missing ICD codes"
-    print "Removed " + str(removed_unk) + " records with unrecognized ICD codes"
-    print "ICD codes not recognized: " + str(notfound)
+    print('Removed', str(removed), 'records with NULL or missing ICD codes')
+    print('Removed', str(removed_unk), 'records with unrecognized ICD codes')
+    print('ICD codes not recognized:', str(notfound))
 
-if __name__ == "__main__":main()
+
+if __name__ == "__main__": main()
